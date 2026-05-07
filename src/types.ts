8739f516693f9dcfe11b27a63f46e2e20288a5fa
Linette Kühn <139412656+linettekuhn@users.noreply.github.com>
2026-05-07 00:46:05 -0400

    Create UI for interacting with caregiver, patient, and shift services

--- src/types.ts
+++ src/types.ts
@@ -12,3 +12,21 @@ export type CreateShiftInput = {
   patient_id: number;
   caregiver_id: number;
 };
+
+export interface Caregiver {
+  id: number;
+  name: string;
+}
+
+export interface Patient {
+  id: number;
+  name: string;
+}
+
+export interface Shift {
+  id: number;
+  start_time: Date;
+  end_time: Date;
+  patient_id: number;
+  caregiver_id: number;
+}
